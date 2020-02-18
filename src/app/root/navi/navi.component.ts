d2d58db1360bc395bf3c0aa6a4be7397f40c684b
taskoff <marian_taskov@abv.bg>
2020-02-18 21:26:04 +0200

    add project

---
+++ src/app/root/navi/navi.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-navi',
+  templateUrl: './navi.component.html',
+  styleUrls: ['./navi.component.css']
+})
+export class NaviComponent implements OnInit {
+  isLoggin: boolean = false;
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit() {
+    // this.elementRef.nativeElement.ownerDocument.body.style.background = 'url(https://images.unsplash.com/photo-1429962714451-bb934ecdc4ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80)';
+    // this.elementRef.nativeElement.ownerDocument.body.style.background = 'url(https://images.unsplash.com/photo-1429962714451-bb934ecdc4ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80)';
+
+  }
+
+}
---
+++ src/app/root/root.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NaviComponent } from './navi/navi.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [NaviComponent, FooterComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [NaviComponent]
+})
+export class RootModule { }
